ace5d1efc204e5c21900806716e0201a59bfa50f
adsidera <annamaria.costalonga@gmail.com>
2016-08-25 12:04:50 +0200

    fixmenutoggle

--- js/site.js
+++ js/site.js
@@ -1,2 +1,12 @@
-
+$("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+        $('.nav-tabs a').click(function(e) {
+  		e.preventDefault();
+  		$(this).tab('show');
+	});
+     $(function() {
+         $("#button").delay(2000).fadeIn(500);
+     });
 
